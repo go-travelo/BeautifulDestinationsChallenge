883a29c786492b6f03643e59e56ae2e8d2b3146c
jordyhers <jordy925@gmail.com>
2022-11-20 20:12:20 +0100

    feat: removed widget_test.dart and added home page widget test.

---
+++ test/home_page.dart
@@ -0,0 +1,43 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:bdb_challenge/constants/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bdb_challenge/main.dart';
+
+void main() {
+
+  var videoKey = const Key(Keys.videoKey);
+
+  group('TikTok video Test', () {
+    testWidgets('When app starts video is visible. ', (WidgetTester tester) async {
+      // pump widget
+      await tester.pumpWidget( MyApp());
+
+      // Verify that video is visible.
+      expect(find.byKey(videoKey), findsOneWidget);
+
+    } );
+
+    testWidgets('User can play and pause video. ', (WidgetTester tester) async {
+      // pump widget
+      await tester.pumpWidget( MyApp());
+      var videoButton = find.byKey(videoKey);
+
+      expect(videoButton, findsOneWidget);
+
+      // Tap the 'video Thumbnail'.
+      await tester.tap(videoButton,warnIfMissed: false,);
+      await tester.pump();
+
+    }
+
+    );
+  });
+
+}
