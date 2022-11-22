75aded799a7359b82f942bd0251d9db870693a48
jordyhers <jordy925@gmail.com>
2022-11-22 18:56:08 +0100

    feat: added import to test file

--- test/home_page.dart
+++ test/home_page.dart
@@ -4,8 +4,7 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:bdb_challenge/constants/constants.dart';
+import 'package:bdb_challenge/src/ui/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bdb_challenge/main.dart';
